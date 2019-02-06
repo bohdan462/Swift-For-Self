a71ea5e4be6285506d8d0a6851c74df932dad873
Bohdan Tkachenko <30517042+bohdan462@users.noreply.github.com>
2019-02-05 19:12:01 -0600

    Add files via upload

---
+++ I AM RICH/I AM RICH/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I AM RICH
+//
+//  Created by student3 on 1/16/19.
+//  Copyright © 2019 Bohdan Tkachenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
